4c712685b531d14e16d0f51930bb9839a7a2963e
dilipsaini123 <138903046+dilipsaini123@users.noreply.github.com>
2024-02-20 11:30:59 +0530

    Update Program.cs

--- Optevus.Ethnicity/Program.cs
+++ Optevus.Ethnicity/Program.cs
@@ -23,12 +23,14 @@ namespace Optevus.Ethnicity
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+          //  if (app.Environment.IsDevelopment())
+           // {
+             //   app.UseSwagger();
+            //    app.UseSwaggerUI();
+          //  }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
